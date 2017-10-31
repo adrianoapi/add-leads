ffb7d9283e69298e9272204d195d340b76e68196
adrianometalslug <sdcomputadores@gmail.com>
2017-10-31 16:57:25 -0200

    add setBr

--- get.php
+++ get.php
@@ -17,12 +17,14 @@ function get_curl()
 class Form
 {
 
+    private $br;
+
     public function campo($formato, $tipo, $id, $name, $indice, $label, $opcoes)
     {
         if ($formato == "input") {
-            return "<label for=\"$indice\">$label<br><input type=\"$tipo\" name=\"$name\" id=\"$indice\"></label>";
+            return "<label for=\"$indice\">{$label}:{$this->br}<input type=\"$tipo\" name=\"$name\" id=\"$indice\"></label>";
         } else {
-            $string = "<label for=\"$indice\">$label<br><select name=\"$name\" id=\"$indice\">";
+            $string = "<label for=\"$indice\">{$label}:{$this->br}<select name=\"$name\" id=\"$indice\">";
             foreach ($opcoes as $key => $value):
                 $string .= "<option vlue=\"{$key}\">{$value}</option>";
             endforeach;
@@ -30,15 +32,21 @@ class Form
         }
     }
 
+    public function setBr($value)
+    {
+        $this->br = $value > 0 ? '<br/>' : NULL;
+    }
+
 }
 
 $form = new Form;
+$form->setBr(true);
 $xml = get_curl();
 
-$input = array('nome', 'signo', 'email', 'telefone', 'curso', 'time');
+$input = array('nome', 'signo', 'email', 'telefone', 'curso', 'time', 'profissionalizantes');
 
 # formato {input/select}
-# tipo {email, number, telphone...}
+# tipo {email, number, password...}
 foreach ($xml as $value):
     # verifica se pode ser impresso
     if (in_array($value->item->name, $input)) {
@@ -47,7 +55,7 @@ foreach ($xml as $value):
         foreach ($value->item[1]->opcoes->opco as $item):
             $opcoes[(int) $item->id] = $item->value;
         endforeach;
-        echo $form->campo($value->item->formato, $value->item->tipo, $value->item->id, $value->item->name, $value->item->indice, $value->item->indice, $opcoes);
+        echo $form->campo($value->item->formato, $value->item->tipo, $value->item->id, $value->item->name, $value->item->indice, $value->item->label, $opcoes);
         echo "<br>";
     }
 endforeach;
