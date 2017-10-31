4d389dc204ade86d0be7130bbfc2247ef7957178
adrianometalslug <sdcomputadores@gmail.com>
2017-10-31 16:06:02 -0200

    add label form

--- get.php
+++ get.php
@@ -17,16 +17,16 @@ function get_curl()
 class Form
 {
 
-    public function campo($formato, $tipo, $id, $name, $indice, $opcoes)
+    public function campo($formato, $tipo, $id, $name, $indice, $label, $opcoes)
     {
         if ($formato == "input") {
-            return "<input type=\"$tipo\" name=\"$name\" id=\"$indice\">";
+            return "<label for=\"$indice\">$label<br><input type=\"$tipo\" name=\"$name\" id=\"$indice\"></label>";
         } else {
-            $string = "<select name=\"$name\" id=\"$indice\">";
+            $string = "<label for=\"$indice\">$label<br><select name=\"$name\" id=\"$indice\">";
             foreach ($opcoes as $key => $value):
                 $string .= "<option vlue=\"{$key}\">{$value}</option>";
             endforeach;
-            return $string .= "</select>";
+            return $string .= "</select></label>";
         }
     }
 
@@ -40,12 +40,14 @@ $input = array('nome', 'signo', 'email', 'telefone', 'curso', 'time');
 # formato {input/select}
 # tipo {email, number, telphone...}
 foreach ($xml as $value):
+    # verifica se pode ser impresso
     if (in_array($value->item->name, $input)) {
+        # gera as opções
         $opcoes = array();
         foreach ($value->item[1]->opcoes->opco as $item):
             $opcoes[(int) $item->id] = $item->value;
         endforeach;
-        echo $form->campo($value->item->formato, $value->item->tipo, $value->item->id, $value->item->name, $value->item->indice, $opcoes);
+        echo $form->campo($value->item->formato, $value->item->tipo, $value->item->id, $value->item->name, $value->item->indice, $value->item->indice, $opcoes);
         echo "<br>";
     }
 endforeach;
