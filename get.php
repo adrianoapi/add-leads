ea22a3724da93bfa97a568a6eacea85e37f7cbc2
adrianometalslug <sdcomputadores@gmail.com>
2017-10-31 16:01:50 -0200

    class form para desenhar o form html

--- get.php
+++ get.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Faz a conexão com a Server e retorna um XML de campos
+ * @return type
+ */
 function get_curl()
 {
     $ch = curl_init();
@@ -10,16 +14,38 @@ function get_curl()
     return simplexml_load_string($result);
 }
 
+class Form
+{
+
+    public function campo($formato, $tipo, $id, $name, $indice, $opcoes)
+    {
+        if ($formato == "input") {
+            return "<input type=\"$tipo\" name=\"$name\" id=\"$indice\">";
+        } else {
+            $string = "<select name=\"$name\" id=\"$indice\">";
+            foreach ($opcoes as $key => $value):
+                $string .= "<option vlue=\"{$key}\">{$value}</option>";
+            endforeach;
+            return $string .= "</select>";
+        }
+    }
+
+}
+
+$form = new Form;
 $xml = get_curl();
 
-$input = array('nome', 'signo','email','telefone','curso','time');
+$input = array('nome', 'signo', 'email', 'telefone', 'curso', 'time');
 
+# formato {input/select}
+# tipo {email, number, telphone...}
 foreach ($xml as $value):
     if (in_array($value->item->name, $input)) {
-        echo "{$value->item->id}, {$value->item->name}";
+        $opcoes = array();
         foreach ($value->item[1]->opcoes->opco as $item):
-            echo "{$item->id} - $item->value ,";
+            $opcoes[(int) $item->id] = $item->value;
         endforeach;
+        echo $form->campo($value->item->formato, $value->item->tipo, $value->item->id, $value->item->name, $value->item->indice, $opcoes);
         echo "<br>";
     }
 endforeach;
