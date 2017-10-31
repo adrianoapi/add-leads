d1427dc197d43ba52e8d7018664cc953dfedf1c6
adrianometalslug <sdcomputadores@gmail.com>
2017-10-31 15:17:43 -0200

    add get método

---
+++ get.php
@@ -0,0 +1,25 @@
+<?php
+
+function get_curl()
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://leads.evolutime.net.br/api/campo/campos?format=xml");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return simplexml_load_string($result);
+}
+
+$xml = get_curl();
+
+foreach ($xml as $value):
+    echo "{$value->item->id}, {$value->item->name}";
+    foreach($value->item[1]->opcoes->opco as $item):
+    echo "{$item->id} - $item->value ,";
+    endforeach;
+    echo "<br>";
+endforeach;
+
+//echo "<pre>";
+//print_r(get_curl());
+//echo "</pre>";
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-function teste_curl($new_name, $new_email)
+function put_curl($new_name, $new_email)
 {
     $username = 'admin';
     $password = '1234';
@@ -26,4 +26,30 @@ function teste_curl($new_name, $new_email)
     return $result;
 }
 
-print_r(teste_curl("adriano", "sdcomputadores@gmail.com"));
+function get_curl($new_name, $new_email)
+{
+    $username = 'admin';
+    $password = '1234';
+
+    $curl_handle = curl_init();
+    curl_setopt($curl_handle, CURLOPT_URL, 'http://leads.evolutime.net.br/api/cadastro/user');
+    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl_handle, CURLOPT_POST, 1);
+    curl_setopt($curl_handle, CURLOPT_POSTFIELDS, array(
+        'token' => "XNSS-HSJW-3NGU-8XTJ",
+        'unidade' => 13,
+        'operador' => 0,
+        'campanha' => 3,
+        'nome' => $new_name,
+        'email' => $new_email
+    ));
+
+    curl_setopt($curl_handle, CURLOPT_USERPWD, $username . ':' . $password);
+
+    $result = curl_exec($curl_handle);
+    curl_close($curl_handle);
+
+    return $result;
+}
+
+//print_r(put_curl("adriano", "sdcomputadores@gmail.com"));
